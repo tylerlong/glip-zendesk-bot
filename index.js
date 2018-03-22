8cb3af085a326327a40b9b69408bcf8949d2ea21
Tyler Long <tyler4long@gmail.com>
2018-03-22 16:20:50 +0800

    Refactor code

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ dotenv.config()
 
 const botId = parseInt(process.env.GLIP_USER_ID)
 let userId
+let groupId
 
 const GlipSocket = require('glip.socket.io')
 const client = new GlipSocket({
@@ -14,109 +15,83 @@ const client = new GlipSocket({
   user: process.env.GLIP_EMAIL,
   password: process.env.GLIP_PASSWORD
 })
-let waiting = false
-let groupId
-client.on('message', (type, data) => {
-  if (type === 2 && data.members.length === 2) {
+
+const postWelcomeMessage = () => {
+  axios.request({
+    url: `https://glip.zendesk.com/api/v2/help_center/en-us/articles/${process.env.ZENDESK_ARTICLE_ID}.json`,
+    method: 'get',
+    auth: {
+      username: process.env.ZENDESK_USERNAME,
+      password: process.env.ZENDESK_PASSWORD
+    }
+  }).then(r => {
+    client.post(groupId, striptags(r.data.article.body))
+  })
+}
+
+const likeUserPost = (postId, postText) => {
+  client.request(
+    `/api/post/${postId}`,
+    'PUT',
+    {
+      'text': postText,
+      '_id': postId,
+      'likes': [
+        botId
+      ]
+    },
+    (error, data) => {
+      console.warn(error)
+      console.log(JSON.stringify(data, null, 2))
+      client.post(groupId, ':smiley:')
+    }
+  )
+}
+
+const createSampleTask = () => {
+  client.request(
+    '/api/task',
+    'POST',
+    {
+      'group_ids': [
+        groupId
+      ],
+      'assigned_to_ids': [
+        userId
+      ],
+      'text': 'A sample task for you',
+      'due': null
+    },
+    (error, data) => {
+      console.warn(error)
+      console.log(JSON.stringify(data, null, 2))
+      client.post(groupId, 'I created a sample task for you, please tick the checkbox to mark it as complete')
+    }
+  )
+}
+
+let waitingForFirstPost = false
+const firstTimeLicenser = (type, data) => {
+  if (type === 2 && data.members.length === 2) { // first time add the bot
     userId = data.members.filter(m => m !== botId)[0]
     groupId = data._id
-    console.log(`New conversation started in group ${groupId}`)
-    axios.request({
-      url: `https://glip.zendesk.com/api/v2/help_center/en-us/articles/${process.env.ZENDESK_ARTICLE_ID}.json`,
-      method: 'get',
-      auth: {
-        username: process.env.ZENDESK_USERNAME,
-        password: process.env.ZENDESK_PASSWORD
-      }
-    }).then(r => {
-      client.post(groupId, striptags(r.data.article.body))
-    })
+    postWelcomeMessage()
     setTimeout(() => {
       client.post(groupId, 'Please post something!')
-      waiting = true
+      waitingForFirstPost = true
     }, 3000)
   }
-  if (waiting && type === 4 && data.creator_id !== botId) {
-    waiting = false
+  if (waitingForFirstPost && type === 4 && data.creator_id !== botId) {
+    waitingForFirstPost = false
     const postId = data._id
     const postText = data.text
-    console.log(data)
-    console.log(postId)
-    console.log(botId)
-    client.request(
-      `/api/post/${postId}`,
-      'PUT',
-      {
-        'text': postText,
-        '_id': postId,
-        'likes': [
-          botId
-        ]
-      },
-      (error, data) => {
-        console.warn(error)
-        console.log(JSON.stringify(data, null, 2))
-        client.post(groupId, ':smiley:')
-      }
-    )
-
-    client.request(
-      '/api/task',
-      'POST',
-      {
-        'group_ids': [
-          groupId
-        ],
-        'assigned_to_ids': [
-          userId
-        ],
-        'text': 'A sample task for you',
-        'due': null
-      },
-      (error, data) => {
-        console.warn(error)
-        console.log(JSON.stringify(data, null, 2))
-        client.post(groupId, 'I created a sample task for you, please tick the checkbox to mark it as complete')
-      }
-    )
+    likeUserPost(postId, postText)
+    setTimeout(() => {
+      createSampleTask()
+    }, 3000)
   }
-})
+}
+
+client.on('message', firstTimeLicenser)
+
 client.start()
-
-// client.on('started', () => {
-//   client.request(
-//     '/api/task',
-//     'POST',
-//     {
-//       'group_ids': [
-//         16130957314
-//       ],
-//       'assigned_to_ids': [
-
-//       ],
-//       'text': 'sample task from code 5',
-//       'due': null
-//     },
-//     (error, data) => {
-//       console.warn(error)
-//       console.log(JSON.stringify(data, null, 2))
-//     }
-//   )
-// })
-
-// const zendesk = require('node-zendesk')
-
-// const z = zendesk.createClient({
-//   username: process.env.ZENDESK_USERNAME,
-//   token: process.env.ZENDESK_TOKEN,
-//   remoteUri: 'https://remote.zendesk.com/api/v2'
-// })
-
-// z.users.list(function (err, req, result) {
-//   if (err) {
-//     console.log(err)
-//     console.log(err.result)
-//     return
-//   }
-//   console.log(JSON.stringify(result[0], null, 2, true))// gets the first page
-// })
