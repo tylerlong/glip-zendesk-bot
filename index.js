94b48a5482543d6fb1dd62e0864532ff5ee97c97
Tyler Long <tyler4long@gmail.com>
2018-03-28 10:30:05 +0800

    Search Zendesk KB

--- index.js
+++ index.js
@@ -71,6 +71,8 @@ const createSampleTask = () => {
 }
 
 let waitingForFirstPost = false
+let waitingForCompleteTask = false
+let finishedFirstTimeWizard = false
 const firstTimeLicenser = (type, data) => {
   if (type === 2 && data.members.length === 2) { // first time add the bot
     userId = data.members.filter(m => m !== botId)[0]
@@ -79,7 +81,7 @@ const firstTimeLicenser = (type, data) => {
     setTimeout(() => {
       client.post(groupId, 'Please post something!')
       waitingForFirstPost = true
-    }, 3000)
+    }, 5000)
   }
   if (waitingForFirstPost && type === 4 && data.creator_id !== botId) {
     waitingForFirstPost = false
@@ -88,10 +90,44 @@ const firstTimeLicenser = (type, data) => {
     likeUserPost(postId, postText)
     setTimeout(() => {
       createSampleTask()
-    }, 3000)
+      waitingForCompleteTask = true
+    }, 5000)
+  }
+  if (waitingForCompleteTask && type === 4 && data.text === '' && data.activity_data && data.activity_data.value === 1) {
+    client.post(groupId, 'Well done!')
+    waitingForCompleteTask = false
+    setTimeout(() => {
+      client.post(groupId, 'You can upload files/images, like this:')
+      client.post_file_from_url(groupId, process.env.SAMPLE_FILE_URL, 'Hey, look at this cool icon!')
+      client.removeListener('message', firstTimeLicenser)
+      finishedFirstTimeWizard = true
+    }, 5000)
   }
 }
 
 client.on('message', firstTimeLicenser)
+
+client.on('message', (type, data) => {
+  if (finishedFirstTimeWizard && type === 4 && data.text.trim() !== '' && data.text.trim().endsWith('?')) {
+    axios.request({
+      url: `https://glip.zendesk.com/api/v2/search.json`,
+      method: 'get',
+      auth: {
+        username: process.env.ZENDESK_USERNAME,
+        password: process.env.ZENDESK_PASSWORD
+      },
+      params: {
+        query: data.text.trim()
+      }
+    }).then(r => {
+      console.log(r.data)
+      if (r.data.results && r.data.results.length > 0) {
+        client.post(groupId, `${striptags(r.data.results[0].body)}\n\n${r.data.results[0].html_url}`)
+      } else {
+        client.post(groupId, 'No result was found.')
+      }
+    })
+  }
+})
 
 client.start()
